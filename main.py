8f98ccc6a61f3b2a880dbb336fcfc486391fbbbc
iayusuf <nomaduk@gmail.com>
2021-07-21 21:27:30 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+answer = 14 / 2
+print(answer)
+answer = 14 % 2
+
+print(answer)
+
+if answer != 0:
+    print("It is an odd number")
+else:
+    print("It is an even number")
---
+++ remainder2.py
@@ -0,0 +1,16 @@
+
+number = int(input("Enter a number: "))
+
+print(f"You have entered {number}")
+
+answer = number / 2
+print(f"Dividing {number} by 2 is {answer}")
+
+answer = number % 2
+
+print(f"The remainder {answer}")
+
+if answer != 0:
+    print(f" {number} is therefore an odd number")
+else:
+    print(f" {number} is therefore an even number")
